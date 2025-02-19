0e1f3032b5e4d5340ebad3dd83713da1d7d7b4e5
waynekjl <kjlwayne@gmail.com>
2025-02-19 18:10:24 +0800

    fixes
    edit androidmanifest.xml and themes.xml
    added astrojumpapp.kt

---
+++ app/src/main/java/edu/singaporetech/astrojump/AstroJumpApp.kt
@@ -0,0 +1,10 @@
+package edu.singaporetech.astrojump
+
+import android.app.Application
+
+class AstroJumpApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize anything needed for your app here
+    }
+}
