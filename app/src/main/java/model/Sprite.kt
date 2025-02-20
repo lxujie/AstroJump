dfa479edc2f74c950d75e726fdef5938e434168e
Liu Xujie <liu_xujie@hotmail.sg>
2025-02-20 19:06:32 +0800

    Updated gitignore

--- app/src/main/java/model/Sprite.kt
+++ app/src/main/java/model/Sprite.kt
@@ -3,14 +3,15 @@ package astrojump.model
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
 
 data class Sprite(
     val image: ImageBitmap,
     var position: MutableState<Offset> = mutableStateOf(Offset.Zero),
-    var rotation: MutableState<Float> = mutableStateOf(0f),
-    var scale: MutableState<Float> = mutableStateOf(1f)
+    var rotation: MutableState<Float> = mutableFloatStateOf(0f),
+    var scale: MutableState<Float> = mutableFloatStateOf(1f)
 ) {
     fun update(deltaTime: Long) {
         position.value = position.value.copy(x = position.value.x + 1f)
